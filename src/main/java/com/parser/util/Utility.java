8f39fc4fb7b6c02207011b89d3221e638fbb9445
shekup <shek.up@gmail.com>
2018-06-17 13:50:54 +0530

    JUNIT

--- src/main/java/com/parser/util/Utility.java
+++ src/main/java/com/parser/util/Utility.java
@@ -3,9 +3,9 @@ package com.parser.util;
 public class Utility {
 	
 	public static String validateLine(String xmlInputLine) throws Exception{
+		System.out.println(xmlInputLine);
 		xmlInputLine = xmlInputLine.replaceAll("\\t", "");
 		xmlInputLine = xmlInputLine.trim();
-		System.out.println(xmlInputLine);
 		int lengthOfLine = xmlInputLine.length();
 		if(!xmlInputLine.startsWith("<") || !xmlInputLine.endsWith(">"))
 			throw new Exception("Formatting exception");
